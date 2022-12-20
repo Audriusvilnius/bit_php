010b655f46e82ff72ed8a3fed64273ed31684140
audriusmacbook <audrius.ivko@gmail.com>
2022-12-20 08:22:40 +0200

    commit

--- bankas_ver_1/seeder.php
+++ bankas_ver_1/seeder.php
@@ -3,8 +3,8 @@ $users = [
     ['name' => 'Bebras', 'psw' => md5('123'), 'color' => 'crimson'],
     ['name' => 'Briedis', 'psw' => md5('123'), 'color' => 'pink'],
     ['name' => 'Paršas', 'psw' => md5('123'), 'color' => 'skyblue'],
-    ['name' => 'Ona', 'psw' => md5('123'), 'color' => 'skyblue'],
-    ['name' => 'Admin', 'psw' => md5('123'), 'color' => 'skyblue']
+    ['name' => 'Ona', 'psw' => md5('123'), 'color' => 'yellow'],
+    ['name' => 'Admin', 'psw' => md5('123'), 'color' => 'green']
 ];
 
 
