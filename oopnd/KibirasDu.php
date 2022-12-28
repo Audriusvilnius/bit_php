ac692fbd8fe100f5f43b1059e05130df9756742f
audriusmacbook <audrius.ivko@gmail.com>
2022-12-28 14:54:54 +0200

    ff

---
+++ oopnd/KibirasDu.php
@@ -0,0 +1,17 @@
+<?php
+class KibirasDu
+{
+    private $pridetakmenu;
+    private $pridetakmeni;
+    public static $akmenusuma;
+
+    public function __construct()
+    {
+        $pridetakmenu = 0;
+        $pridetakmeni = 0;
+    }
+
+    public function __get($name)
+    {
+    }
+}
