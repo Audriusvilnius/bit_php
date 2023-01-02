ca2e6412333f1d0dc6558b743a05666362afcca7
audriusmacbook <audrius.ivko@gmail.com>
2023-01-02 13:32:59 +0200

    j

--- front/public/index.php
+++ front/public/index.php
@@ -2,7 +2,9 @@
 
 use Front\App;
 
-define('URL', 'http://front.lt/');
+//define('URL', 'http://front.lt/');
+define('URL', 'http://localhost/bit_php/front/public/');
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
