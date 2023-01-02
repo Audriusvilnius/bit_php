f796b7e36b3c38621512508eb7f78c1d6ba48f7a
audriusmacbook <audrius.ivko@gmail.com>
2023-01-02 15:49:04 +0200

    019

--- 019/index.php
+++ 019/index.php
@@ -7,18 +7,18 @@ require __DIR__ . '/Briedis.php';
 
 
 
-// $briedis1 = new Briedis('crimson');
-// $briedis2 = new Briedis('green');
+// // $briedis1 = new Briedis('crimson');
+// // $briedis2 = new Briedis('green');
 
-echo $briedis1->color = 'yellow';
+// echo $briedis1->color = 'yellow';
 
-echo '<br>';
-echo $briedis1->blabla;
-echo '<br>';
+// echo '<br>';
+// echo $briedis1->blabla;
+// echo '<br>';
 
-// echo $briedis1;
+// // echo $briedis1;
 
-echo $briedis1('GERAS');
+// echo $briedis1('GERAS');
 
 print_r($briedis1);
 print_r($briedis2);
