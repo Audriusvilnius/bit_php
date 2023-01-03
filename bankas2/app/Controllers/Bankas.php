ae2b632e4dbf6291da397501909c0e520e428daa
audriusmacbook <audrius.ivko@gmail.com>
2023-01-03 13:04:09 +0200

    BAnkas

---
+++ bankas2/app/Controllers/Bankas.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bankas\Controllers;
+
+use Bankas\App;
+use Bankas\DB\FileReader as FR;
+
+class Grybas
+{
+
+    public function index()
+    {
+        $grybai = (new FR('grybai'))->showAll();
+        $pageTitle = 'Grybai | Sąrašas';
+        return App::view('grybas-list', compact('grybai', 'pageTitle'));
+    }
+
+    public function create()
+    {
+        $pageTitle = 'Grybai | Naujas';
+        return App::view('grybas-create', compact('pageTitle'));
+    }
+
+    public function save()
+    {
+        (new FR('grybai'))->create($_POST);
+        return App::redirect('grybai');
+    }
+
+    public function edit($id)
+    {
+        $pageTitle = 'Grybai | Redaguoti';
+        $grybas = (new FR('grybai'))->show($id);
+        return App::view('grybas-edit', compact('pageTitle', 'grybas'));
+    }
+
+    public function update($id)
+    {
+        (new FR('grybai'))->update($id, $_POST);
+        return App::redirect('grybai');
+    }
+
+    public function delete($id)
+    {
+        (new FR('grybai'))->delete($id);
+        return App::redirect('grybai');
+    }
+}
--- bankas2/public/index.php
+++ bankas2/public/index.php
@@ -1,105 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>BANKAS RAGAI IR KANOPOS</title>
-</head>
-<header class="p-3 bg-dark text-white">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-            </a>
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="http://bankasdu.lt/index.php" class="nav-link px-2 text-white"><img style="filter: invert(70%);" class="d-block  pb-6" src="./home.png" alt="pig" height="45"></a></li>
-                <!-- <li><a href="http://localhost/bit_php/bankas_ver_1/new.php" class="nav-link px-2 text-white p-4">Add Account</a></li>
-                <li><a href="http://localhost/bit_php/bankas_ver_1/transfer.php" class="nav-link px-2 text-white p-4">Transfer</a></li>
-                <li><a href="http://localhost/bit_php/bankas_ver_1/allcustomers.php" class="nav-link px-2 text-white p-4">Customers</a></li> -->
-                <!-- <li><a href="https://www.youtube.com/watch?v=OfqVGcBeqyQ" class="nav-link px-2 text-white p-4"><img src="./pornhub_logo_straight.svg" height="18"></li></a> -->
-            </ul>
-            <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-            </form> -->
+use Bankas\App;
 
-            <div class="text-end">
-                <button type="button" class="btn btn-outline-light me-2">Log in </button>
-                <!-- <button type="button" class="btn btn-warning">Sign-up</button> -->
-            </div>
-        </div>
-        <h2 class="pb-4 border-bottom"></h2>
-    </div>
-</header>
+define('URL', 'http://bankasdu.lt.lt/');
 
-<body class="p-3 bg-dark text-white">
-    <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Tavo svajoniu Bankas!</h1>
-        <img style="filter: invert(70%);" class="d-block mx-auto mb-4" src="./img_464486.png" alt="pig" height="357">
-        <h2 class="display-5 fw-bold p-8">"Ragai ir Kanopos"</h2>
-        <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus error
-                reprehenderit assumenda repellat molestias quo voluptatum fuga voluptas. Ipsa vel possimus tempora
-                fugit delectus amet tempore sint enim ratione sed!</p>
-            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <form action=" http://localhost/bit_php/bankas_ver_1/new.php" method="GET">
+require __DIR__ . '/../vendor/autoload.php';
 
-                    <button type="submit" class="btn btn-warning btn-lg px-4 gap-3">Pradet</button>
-                </form> <!-- <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button> -->
-            </div>
-        </div>
-    </div>
 
-</body>
-<footer>
-    <div class="container ">
-        <h4 class="pb-4 border-bottom">Kontaktai</h4>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
-            <div class="col d-flex align-items-start">
-                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-                <div class="fs-6">
-                    <h5 class="fw-bold mb-0">Adresas</h5>
-                    Trečia eglė už pušies.
-                    LT-9giles, Labanoro giria.
-                </div>
-            </div>
-            <div class="col d-flex align-items-start">
-                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                    <use xlink:href="#cpu-fill"></use>
-                </svg>
-                <div>
-                    <h5 class="fw-bold mb-0">Telefonai</h5>
-                    <p>Skambinti varpasi</p>
-                </div>
-            </div>
-            <div class="col d-flex align-items-start">
-                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                    <use xlink:href="#calendar3"></use>
-                </svg>
-                <div>
-                    <h5 class="fw-bold mb-0 ">Įmonės informacija</h5>
+$response = App::start();
 
-                    <p>2023 Metu gazėlė.</p>
-                </div>
-            </div>
-            <div class="col d-flex align-items-start">
-                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                    <use xlink:href="#home"></use>
-                </svg>
-                <div>
-                    <h5 class="fw-bold mb-0">El. pašto adresai</h5>
-
-                    <p>ragai.ir.kanopos@bit.lt.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-
-</html>
+echo $response;
