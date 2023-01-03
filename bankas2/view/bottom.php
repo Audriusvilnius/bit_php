b2418b0310b2083ab3a49bdb166bbc79807368fe
audriusmacbook <audrius.ivko@gmail.com>
2023-01-03 12:39:42 +0200

    index

--- bankas2/view/bottom.php
+++ bankas2/view/bottom.php
@@ -1,3 +1,49 @@
 </body>
+<footer>
+    <div class="container ">
+        <h4 class="pb-4 border-bottom">Kontaktai</h4>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+            <div class="col d-flex align-items-start">
+                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+                <div class="fs-6">
+                    <h5 class="fw-bold mb-0">Adresas</h5>
+                    Trečia eglė už pušies.
+                    LT-9giles, Labanoro giria.
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <use xlink:href="#cpu-fill"></use>
+                </svg>
+                <div>
+                    <h5 class="fw-bold mb-0">Telefonai</h5>
+                    <p>Skambinti varpasi</p>
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <use xlink:href="#calendar3"></use>
+                </svg>
+                <div>
+                    <h5 class="fw-bold mb-0 ">Įmonės informacija</h5>
+
+                    <p>2023 Metu gazėlė.</p>
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <use xlink:href="#home"></use>
+                </svg>
+                <div>
+                    <h5 class="fw-bold mb-0">El. pašto adresai</h5>
+
+                    <p>ragai.ir.kanopos@bit.lt.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
 </html>
