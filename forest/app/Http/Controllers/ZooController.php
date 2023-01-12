a998436905f3f7b9363c7478fed9beb54da6fbb9
audriusmacbook <audrius.ivko@gmail.com>
2023-01-12 12:21:48 +0200

    ss

--- forest/app/Http/Controllers/ZooController.php
+++ forest/app/Http/Controllers/ZooController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class ZooController extends Controller
 {
-    public function enter($id){
-        return 'Labas is kontrolerio Nr.:'.$id;
+    public function enter(Request $request, $id){
+
+//http://localhost/bit_php/forest/public/zoo/enter/88?v=aaa
+//http://localhost/bit_php/forest/public/zoo/enter/88?v=aaa&b=uuuu
+
+        return 'Versija '.$request->v.' ' .$request->b.'Labas is kontrolerio Nr.:'.$id;
     }
 }
